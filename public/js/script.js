3698335d1715a3d3f1d960eaf5f261d5d76f98d3
aceslowman <aceslowman@gmail.com>
2017-07-11 18:52:16 -0600

    Updated

--- public/js/script.js
+++ public/js/script.js
@@ -34,5 +34,11 @@ function render(){
   renderer.render( scene, camera );
 }
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 init();
 render();
